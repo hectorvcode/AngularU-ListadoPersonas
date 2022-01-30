6ad8b15fcb7d7c0f9e39f3f2f0ead96acca0931d
hectorvcode <hectorfabiovasquez@gmail.com>
2022-01-29 21:48:36 -0500

    Added ngFor directive example

---
+++ src/app/persona.model.ts
@@ -0,0 +1,16 @@
+export class Persona {
+
+    /**Esto se puede simplificar con el constructor pùblico
+    nombre: string = '';
+    apellido: string = '';
+
+    constructor(nombre: string, apellido: string){
+        this.nombre = nombre;
+        this.apellido = apellido
+    }
+    */
+
+    constructor(public nombre:string, public apellido: string){
+        
+    }
+}
